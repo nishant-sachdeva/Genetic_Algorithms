82288576c6e3dd43c3c6b162e079698dac1599a8
nishant-sachdeva <nishant.sachdeva@research.iiit.ac.in>
2020-03-24 16:48:51 +0530

    some more changes

--- geneticFunctions.py
+++ geneticFunctions.py
@@ -1,85 +1,77 @@
 import client_moodle
 import random
 import statistics
-# import constants 
 
 def naturalSelection(population, populationSize, private_key):
-    prevGenFitnessTrain = []
-
-    prevGenFitnessValidation = []
-
-
+    prevGenFitnessTrain=[]
+    prevGenFitnessValidation=[]
     for i in range(0, populationSize):
-        try:
-            temp = client_moodle.get_errors(private_key, population[i])
-            print(str(i)+"th element's error for train and validation are " +str(temp[0])+" "+str(temp[1]))
-        except:
-            print("natural selection call did not work")
-            temp = [1e40 , 1e40]
-        
+        temp=client_moodle.get_errors(private_key, population[i])
+        print(str(i)+"th element's error for train and validation are " +str(temp[0])+" "+str(temp[1]))
         prevGenFitnessTrain.append(temp[0])
-
         prevGenFitnessValidation.append(temp[1])
-   
-    # trainGuidelineUpper = 3625792
-    # trainGuidelineLower = 79569
-    # validationGuidelineUpper = 3625792
+    trainGuidelineUpper = 3625792
+    trainGuidelineLower = 79569
+    validationGuidelineUpper = 3625792
 
-    # population = [x for _, x in sorted(zip(prevGenFitnessTrain, population))] 
+    population = [x for _, x in sorted(zip(prevGenFitnessValidation, population))]
+    sortedFitnessValArray = sorted(prevGenFitnessValidation) 
     
+    fittestIndividualsForDirect = [i for i in range(0, int(populationSize/10))]
+    fittestIndividualsForCrossing = [i for i in range(0, int(populationSize/5))]
+
+    return fittestIndividualsForDirect, fittestIndividualsForCrossing, sortedFitnessVal
     ####median method
     
-    medTrainVal = statistics.median(prevGenFitnessTrain)
+    # medTrainVal = statistics.median(prevGenFitnessTrain)
+    # medValidationVal = statistics.median(prevGenFitnessValidation)
+    # indexTrain=[]
+    # indexValidation=[]
+    # for i in range(0, populationSize):
+    #     if prevGenFitnessTrain[i]>medTrainVal:
+    #         indexTrain.append(i)
+    #     if prevGenFitnessValidation[i]<medValidationVal:
+    #         indexValidation.append(i)
+    # commonIndices = list(set(indexTrain).intersection(set(indexValidation)))
+    # return commonIndices, indexTrain, indexValidation, prevGenFitnessTrain, prevGenFitnessValidation
 
-    medValidationVal = statistics.median(prevGenFitnessValidation)
-
-    indexTrain=[]
-
-    indexValidation=[]
-
-    for i in range(0, populationSize):
-        if prevGenFitnessTrain[i] > medTrainVal:
-            indexTrain.append(i)
-        if prevGenFitnessValidation[i] < medValidationVal:
-            indexValidation.append(i)
-    commonIndices = list(set(indexTrain).intersection(set(indexValidation)))
-
-    return commonIndices, indexTrain, indexValidation, prevGenFitnessTrain, prevGenFitnessValidation 
-
-def crossover(population, nextGenPopulation, populationSize, commonIndices, indexTrain, indexValidation):
-    
-    if len(commonIndices) > 0:
-        for i in range(0, len(commonIndices)):
-            nextGenPopulation.append(population[commonIndices[i]])
-
-        symmetricDifferenceIndex = list(set(indexTrain).symmetric_difference(set(indexValidation)))
-
-        for i in range(0, int((populationSize-len(commonIndices))/2)):
-            tempArray = mate(population, symmetricDifferenceIndex)
-            nextGenPopulation.append(tempArray[0])
-            nextGenPopulation.append(tempArray[1])
-
-        if len(nextGenPopulation)<populationSize:
-            nextGenPopulation.append(mate(population, symmetricDifferenceIndex)[0])
-    
-    else:    
-        unionIndex = list(set(indexTrain).union(set(indexValidation)))
-        for i in range(0, int(populationSize/2)):
-            tempArray = mate(population, unionIndex)
-            nextGenPopulation.append(tempArray[0])
-            nextGenPopulation.append(tempArray[1])
+def crossover(population, nextGenPopulation, populationSize, fittestIndividualsForDirect, fittestIndividualsForCrossing):
+    for i in range(0, len(fittestIndividualsForDirect)):
+        nextGenPopulation.append(population[fittestIndividualsForDirect[i]])
+        
+    for i in range(0, (populationSize-len(fittestIndividualsForDirect))/2):
+        tempArray = mate(population, fittestIndividualsForCrossing)
+        nextGenPopulation.append(tempArray[0])
+        nextGenPopulation.append(tempArray[1])
+    if len(nextGenPopulation)!=populationSize:
+        nextGenPopulation.append(fittestIndividualsForDirect[0])
     return nextGenPopulation
+    # if len(commonIndices)>0:
+    #     for i in range(0, len(commonIndices)):
+    #         nextGenPopulation.append(population[commonIndices[i]])
+    #     symmetricDifferenceIndex=list(set(indexTrain).symmetric_difference(set(indexValidation)))
+    #     for i in range(0, int((populationSize-len(commonIndices))/2)):
+    #         tempArray = mate(population, symmetricDifferenceIndex)
+    #         nextGenPopulation.append(tempArray[0])
+    #         nextGenPopulation.append(tempArray[1])
+    #     if len(nextGenPopulation)<populationSize:
+    #         nextGenPopulation.append(mate(population, symmetricDifferenceIndex)[0])
+    # else:
+    #     unionIndex=list(set(indexTrain).union(set(indexValidation)))
+    #     for i in range(0, int(populationSize/2)):
+    #         tempArray = mate(population, unionIndex)
+    #         nextGenPopulation.append(tempArray[0])
+    #         nextGenPopulation.append(tempArray[1])
+    # return nextGenPopulation
 
 
 def mate(population, symmetricDifferenceIndex):
-
     a = random.randint(0, len(symmetricDifferenceIndex)-1)
     b = random.randint(0, len(symmetricDifferenceIndex)-1)
-    while b == a:
-        b = random.randint(0, len(symmetricDifferenceIndex)-1)
+    while b==a:
+        b=random.randint(0, len(symmetricDifferenceIndex)-1)
     offSpring0=[]
     offSpring1=[]
-    
     #####uniform crossover
 
     # for i in range(0, 11):
@@ -93,60 +85,61 @@ def mate(population, symmetricDifferenceIndex):
     
     #####single-point crossover
 
-    crossoverPoint = random.randint(0, 10)
-    for i in range(0, 11):
-        if i < crossoverPoint:
-            offSpring0.append(population[a][i])
-            offSpring1.append(population[b][i])
-        else:
-            offSpring0.append(population[b][i])
-            offSpring1.append(population[a][i])
+    # crossoverPoint=random.randint(0, 10)
+    # for i in range(0, 11):
+    #     if i < crossoverPoint:
+    #         offSpring0.append(population[a][i])
+    #         offSpring1.append(population[b][i])
+    #     else:
+    #         offSpring0.append(population[b][i])
+    #         offSpring1.append(population[a][i])
 
     #####double-point crossover
 
-    # crossoverPoint0 = random.randint(0, 9)
-    # crossoverPoint1 = random.randint(crossoverPoint0+1, 10)
-    # for i in range(0, 11):
-    #     if i < crossoverPoint0:
-    #         offSpring0.append(population[a][i])
-    #         offSpring1.append(population[b][i])
-    #     elif i >= crossoverPoint0 and i < crossoverPoint1:
-    #         offSpring0.append(population[b][i])
-    #         offSpring1.append(population[a][i])
-    #     else:
-    #         offSpring0.append(population[a][i])
-    #         offSpring1.append(population[b][i])
-
+    crossoverPoint0 = random.randint(0, 9)
+    crossoverPoint1 = random.randint(crossoverPoint0+1, 10)
+    for i in range(0, 11):
+        if i < crossoverPoint0:
+            offSpring0.append(population[a][i])
+            offSpring1.append(population[b][i])
+        elif i >= crossoverPoint0 and i < crossoverPoint1:
+            offSpring0.append(population[b][i])
+            offSpring1.append(population[a][i])
+        else:
+            offSpring0.append(population[a][i])
+            offSpring1.append(population[b][i])
     offSprings = []
     offSprings.append(offSpring0)
     offSprings.append(offSpring1)
     return offSprings
 
 def mutate(nextGenPopulation, populationSize):
-
-    # int mutate_these_many = random.randrange(0, populationSize)
-    
-    # An alternate way of mutation : slightly less often ( it seems , but def more random)
-
-    # for i in range(0, mutate_these_many):
-    #     guy = random.randrange(0, populationSize)
-    #     # so we mutate this Guy'th dude
-    #     # in this guyth dude, we shall make some random number of genes
-
-    #     int change_these_many_genes = random.randrange(0,11)
-
-    #     for j in range(0,change_these_many_genes):
-    #         gene = random.randrange(0,3)
-    #         # now for this particular gene
-    #         nextGenPopulation[guy][gene] = random.uniform(-10, 10)
-
-    # return nextGenPopulation
-
     for i in range(0, populationSize):
         coin = random.uniform(0, 1)
         if coin > 0.95:
             for j in range(0, 11):
                 coin1=random.uniform(0, 1)
-                if coin1 > 0.95:
+                if coin1>0.95:
                     nextGenPopulation[i][j]=random.uniform(-10, 10)
     return nextGenPopulation
+
+
+def storeBestGeneration(population, bestErrorValOfGeneration):
+    f2 = open("bestErrorVal.txt", "r+")
+    f3 = open("bestPopulation.txt", "w+")
+
+    bestErrorVal=f2.read()
+
+    if len(bestErrorVal)!=0:
+        
+        bestErrorVal=int(bestErrorVal)
+        
+        if bestErrorVal>bestErrorValOfGeneration:
+            bestErrorVal=bestErrorValOfGeneration
+            f3.write(str(population))
+
+    else:
+        bestErrorVal = bestErrorValOfGeneration
+        f3.write(str(population))
+    f3.close()
+    f2.close()
--- genetic_algorithm.py
+++ genetic_algorithm.py
@@ -31,29 +31,44 @@ print(num)
 
 population = []
 
-# try:
-with open("data_pickle.txt", "rb") as new_filename:
-    pp = pickle.load(new_filename)
-    # with open(data.txt) as f:
-    #     population = json.load(f)
+try:
+    with open('data.txt') as new_filename:
+        population = json.load(new_filename)
 
-# except:
-#     print("the file did not load :/")
-#     for i in range(3):
-#         population.append(num)
+except:
+    print("the file did not load :/")
+    for i in range(3):
+        population.append(num)
 
-#     for i in range(0, populationSize-3):
-#         person = []
-#         for i in range(0, 11):
-#             gene = random.uniform(-10, 10)
-#             person.append(gene)
-#         population.append(person)
+    for i in range(0, populationSize-3):
+        person = []
+        for i in range(0, 11):
+            gene = random.uniform(-10, 10)
+            person.append(gene)
+        population.append(person)
 
 
 
-# # nextGenPopulation = []
+nextGenPopulation = []
 
-# # print("Generation: 0")
+# print("Generation: 0")
+for i in range(1, 100):
+
+    print("Generation Number is " + str(i))
+    fittestIndividualsForDirect, fittestIndividualsForCrossing, sortedFitnessValArray=geneticFunctions.naturalSelection(population, populationSize, private_key)
+
+    nextGenPopulation=geneticFunctions.crossover(population, nextGenPopulation, populationSize, fittestIndividualsForDirect, fittestIndividualsForCrossing)
+
+    nextGenPopulation=geneticFunctions.mutate(nextGenPopulation, populationSize)
+
+    # print(population)
+
+    population=nextGenPopulation
+
+    geneticFunctions.storeBestGeneration(population, sortedFitnessValArray[0])
+    # print(str(statistics.median(thisGenTrain))+" "+str(statistics.median(thisGenValidation)))
+    # print("Generation: "+str(i), end=" ")
+
 
 # for i in range(1, 4):
 #     print("generation "+ str(i))
@@ -67,13 +82,13 @@ with open("data_pickle.txt", "rb") as new_filename:
 
 
 
-# # I have to find a way to make this population persistent
-# try:
-#     with open(data.txt , "w") as f:
-#         json.dump(population , f)
+# I have to find a way to make this population persistent
+try:
+    with open('data.txt' , "w") as f:
+        json.dump(population , f)
 
-# except:
-#     print("the file did not load :/")
-#     print(population)
+except:
+    print("the file did not load :/")
+    print(population)
 
-# # this code should write my file into the j
+# this code should write my file into the j
